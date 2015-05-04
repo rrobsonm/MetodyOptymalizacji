e51e741f9d4e188429c047eb680a22e4ea636ba7
Adam Lach <rrobsonm@gmail.com>
2015-05-04 13:04:45 +0200

    Creation of classes, and basic methods

---
+++ src/scheduleOptimalizator/Population.java
@@ -0,0 +1,14 @@
+package scheduleOptimalizator;
+
+import java.util.List;
+
+public class Population {
+
+	int size;
+	int elite;
+	List<Sollution> sollutions;
+	
+	public void evolve(){
+		
+	}
+}
---
+++ src/scheduleOptimalizator/StudentList.java
@@ -0,0 +1,29 @@
+package scheduleOptimalizator;
+
+public class StudentList extends Sollution {
+
+	@Override
+	protected void generate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Sollution cross(Sollution sollution) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Sollution mutate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int valuate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
