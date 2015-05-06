c86c6da56ec5f7be33d9558c343b50908bf51c10
Adam Lach <rrobsonm@gmail.com>
2015-05-06 22:10:08 +0200

    TimeProvider creation

--- src/scheduleOptimalizator/Student.java
+++ src/scheduleOptimalizator/Student.java
@@ -1,5 +1,17 @@
 package scheduleOptimalizator;
 
-public class Student {
+import java.util.List;
 
+public class Student {
+	
+	int id;
+	int[] requiredClasses;
+	int[] travelTime;
+	
+	public int getTravelTime(int timeSlotNumber){
+		int minutes=0;
+		
+		return minutes;
+	}
+	
 }
---
+++ src/scheduleOptimalizator/TimeProvider.java
@@ -0,0 +1,23 @@
+package scheduleOptimalizator;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class TimeProvider {
+	
+	private static final int slotLength=5;
+
+	public static int timeToTimeSlotNumber(DayOfWeek day, LocalTime time){
+		int timeSlotNumber=0;
+		
+		if(60%slotLength != 0) throw new IllegalArgumentException("slotLength problem");
+		
+		if(time.getSecond() != 0) throw new IllegalArgumentException("time in timeToTimeSlotNumber musn't has seconds");
+		
+		timeSlotNumber = (day.getValue()-1)*24*60/slotLength;
+		timeSlotNumber = timeSlotNumber + time.getHour()*60/slotLength;
+		timeSlotNumber = timeSlotNumber + time.getMinute()/slotLength;
+		
+		return timeSlotNumber;
+	}
+}
